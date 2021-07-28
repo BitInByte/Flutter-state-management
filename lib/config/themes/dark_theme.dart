889a50f41de5604ff40d20a0a0dad867c7b29bf2
BitInByte <bitinbyte@outlook.com>
2021-07-28 15:58:25 +0100

    Improved Theme

--- lib/config/themes/dark_theme.dart
+++ lib/config/themes/dark_theme.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+/* class DarkTheme { */
+/* static ThemeData theme = ThemeData( */
+/* brightness: Brightness.dark, */
+/* primarySwatch: Colors.pink, */
+/* textTheme: TextTheme( */
+/* headline6: TextStyle( */
+/* color: Colors.black, */
+/* ), */
+/* subtitle1: TextStyle( */
+/* color: Colors.black87, */
+/* ), */
+/* ), */
+/* ); */
+/* } */
+
 class DarkTheme {
-  static ThemeData theme = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: Colors.pink,
+  static ThemeData theme = ThemeData.dark().copyWith(
+    primaryColor: Colors.pink,
+    iconTheme: IconThemeData(
+      color: Colors.pink,
+    ),
+    buttonTheme: ButtonThemeData().copyWith(
+      buttonColor: Colors.pink,
+      textTheme: ButtonTextTheme.accent,
+    ),
+    buttonColor: Colors.pink,
     textTheme: TextTheme(
       headline6: TextStyle(
         color: Colors.black,
